fa05eb16bac66374e1b702275fcc81b261006915
sangreashim <38829486+sangreashim@users.noreply.github.com>
2018-04-29 16:22:27 +0900

    Update NGS_QF.sh

--- NGS_QF.sh
+++ NGS_QF.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/sh
 
 ## argv1 is directory stores fastq.gz files
-## argv2 is directory stores IlluQC.pl
+## argv2 is full path of IlluQC.pl
 
-for file_name in 'V_nepal_1_' 'V_nipp_1_'
+cd $1
+ls *.fastq.gz | cut -f 1 -d '_' | sort -u > fastq_header
+
+for file_name in $(cat "fastq_header")
 do
-
 echo $file_name' is now on quality filering!!'
-
-ngstoolkit_dir=$(echo $file_name'ngstoolkit')
+ngstoolkit_dir=$(echo $file_name'_ngstoolkit')
 echo $ngstoolkit_dir
-
-mkdir $1/$ngstoolkit_dir
-
-first_file=$1/${file_name}'1.fastq.gz'
-second_file=$1/${file_name}'2.fastq.gz'
-
-perl $2 -pe $first_file $second_file N A -o $1/$ngstoolkit_dir -p 4 -t 2 -z g
+mkdir $ngstoolkit_dir
+first_file=${file_name}'_1.fastq.gz'
+second_file=${file_name}'_2.fastq.gz'
+perl $2 -pe $first_file $second_file N A -o $ngstoolkit_dir -p 4 -t 2 -z g
 done
+
+cd ..
