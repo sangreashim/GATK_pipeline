bb631dc1c452383b0f7bf11287d609a41c3ebded
sangreashim <38829486+sangreashim@users.noreply.github.com>
2018-04-29 16:24:58 +0900

    Update NGS_QC.sh

--- NGS_QC.sh
+++ NGS_QC.sh
@@ -4,17 +4,11 @@
 
 for i in `ls $1`
 do
-
 echo $i ' is now quality checking!!'
-
 file_name=$(echo $i |cut -d '.' -f 1)
 echo $file_name
-
 fastqc_dir=${file_name}'_fastqc'
 echo $fastqc_dir
-
 mkdir $1/$fastqc_dir
-
 fastqc $1/$i -o $1/$fastqc_dir/
-
 done
