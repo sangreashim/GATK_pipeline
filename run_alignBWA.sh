2c6df137855dc2e87cad5903623f565d8e3cc659
sangreashim <38829486+sangreashim@users.noreply.github.com>
2018-04-29 16:42:05 +0900

    Update run_alignBWA.sh

--- run_alignBWA.sh
+++ run_alignBWA.sh
@@ -1,36 +1,14 @@
-#	$1	bwa_reference
-#	$2	cohort
-#	$3	NGS_dir
+#       $1      bwa_reference
+#       $2      NGS_dir
 
-bwa index $1 $1
+#bwa index $1 $1
+cd $2
 
-mkdir $2
-cd $3
-
+for file_name in `ls *.fastq.gz | cut -f 1 -d '_' | sort -u`
+do
 echo '##################################### Mapping '$2' #####################################' 
-
-paired1=$(echo $2'_1.fastq.gz')
-paired2=$(echo $2'_2.fastq.gz')
-
-bwa mem -t 4 -M $1 $paired1 $paired2 | samtools view -bS - > ../$2/$2.bam
-
+paired1=$file_name'_1.fastq.gz'
+paired2=$file_name'_2.fastq.gz'
+bwa mem -t 4 -M $1 $paired1 $paired2 | samtools view -bS - > $file_name.bam
+done
 cd ../
-
-cp gatk_pipe.sh $2/
-cp gatk_pipe_variation_validation.sh $2/
-cp run_varTyping.sh $2/
-cp run_stat.sh $2/
-
-#bwa sampe ../ref/redbean.ref.fasta ./V_nakashimae_2_1.fastq.gz.sai ./V_nakashimae_2_2.fastq.gz.sai ./V_nakashimae_2_1.fastq.gz ./V_nakashimae_2_2.fastq.gz | samtools view -bS - > ./V_nakashimae_2_1.bam
-
-echo '#####################################'
-
-#samtools faidx ../redbean.ref.fasta
-
-#echo '#####################################'
-
-#samtools import /data1/kev8305/ysp_resequencing/redbean.ref.fasta.fai ./ysp-2.fastq.gz.sam ./ysp-2.fastq.gz.sam.bam
-
-#echo '#####################################'
-
-
