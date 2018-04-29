b945e1e3c9e81f10212edfc74ad569dfa90958af
sangreashim <38829486+sangreashim@users.noreply.github.com>
2018-04-29 17:29:37 +0900

    Update gatk_pipe_variation_validation.sh

--- gatk_pipe_variation_validation.sh
+++ gatk_pipe_variation_validation.sh
@@ -4,9 +4,7 @@
 #	$4	reference.fa
 #	$5	maxDP == averageCov*2
 
-python ~/pythonCode/\[vcf\]depth_filter_allPosition.py $1 0 $5 
-
 echo 'gaTK filter variation from '$1'. reference is '$4
 
-java -Djava.io.tmpdir=./temp/ -Xmx18g -jar $3/GenomeAnalysisTK.jar -R $4 -T VariantFiltration --variant $1.DPflt -o $1.DPflt.filtered --filterExpression "MQ0 >= 4 && ((MQ0 / (1.0*DP))>0.1)" --filterName "HARD_TO_VALIDATE" --filterExpression "QUAL<30" --filterName "QualFilter"
-grep 'PASS' $1.DPflt.filtered > $1.DPflt.filtered.passed
+java -Djava.io.tmpdir=./temp/ -Xmx18g -jar $3/GenomeAnalysisTK.jar -R $4 -T VariantFiltration --variant $1 -o $1.filtered --filterExpression "MQ0 >= 4 && ((MQ0 / (1.0*DP))>0.1)" --filterName "HARD_TO_VALIDATE" --filterExpression "QUAL<30" --filterName "QualFilter"
+grep 'PASS' $1.filtered > $1.filtered.passed
