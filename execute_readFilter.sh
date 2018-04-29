dd1bba4148de70362810db122fa5fbb169ee5ac1
sangreashim <38829486+sangreashim@users.noreply.github.com>
2018-04-29 16:29:24 +0900

    Update execute_readFilter.sh

--- execute_readFilter.sh
+++ execute_readFilter.sh
@@ -1,3 +1,5 @@
-sh NGS_QC.sh ./NGS 2> NGS_QC.sh.out
+# $1 directory of NGS file saved
+# $2 full path of IlluQC.pl
 
-sh NGS_QF.sh ./NGS /data1/program/NGSQCToolkit_v2.3.3/QC/IlluQC.pl 2> NGS_QF.sh.out
+sh NGS_QC.sh $1 2> NGS_QC.sh.out
+sh NGS_QF.sh $1 $2 2> NGS_QF.sh.out
